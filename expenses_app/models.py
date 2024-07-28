33adb3ca03cccb89712a3367ced7d15414c2a07e
keshav_regmi <keshavregmi70@gmail.com>
2024-07-28 19:39:04 +0530

    Created Model

--- expenses_app/models.py
+++ expenses_app/models.py
@@ -1,3 +1,34 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=255)
+    mobile_number = models.CharField(max_length=15)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'name', 'mobile_number']
+
+
+class Expense(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='expenses')
+    description = models.CharField(max_length=255)
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateField(auto_now_add=True)
+    split_type = models.CharField(max_length=20, choices=[
+        ('equal', 'Equal'),
+        ('exact', 'Exact'),
+        ('percentage', 'Percentage')
+    ])
+
+
+class Split(models.Model):
+    expense = models.ForeignKey(
+        Expense, related_name='splits', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.DecimalField(
+        max_digits=10, decimal_places=2, null=True, blank=True)
+    percentage = models.DecimalField(
+        max_digits=5, decimal_places=2, null=True, blank=True)
